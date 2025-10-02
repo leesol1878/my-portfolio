be1acfe457144f70b160bfad30a632905f5239af
leesol1878 <stsionastw@gmail.com>
2025-10-02 14:59:31 +0300

    added chala

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,5 +10,7 @@ export const Home = () => {
     {/* Main Content */}
 
     {/* Footer */}
+    chala
     </div>;
+    
 };
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwind from "@tailwindcss/vite"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': pathresolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
